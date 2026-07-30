bbb5598509199a85f6ca5d138cf36320e223ca82
David Bilan <bilandavid07@gmail.com>
2026-07-31 00:42:29 +0200

    feat: trust the headers of the proxy, so that requet.url resolves to https

--- flask_app.py
+++ flask_app.py
@@ -8,7 +8,15 @@ from fts_querry_prefix import fts_prefix_query
 
 from sorting_order_enum import SortingOrder
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 app = Flask(__name__)
+
+app.wsgi_app = ProxyFix(
+    app.wsgi_app,
+    x_proto=1,
+    x_host=1
+)
 
 TRIPS_DATABASE = 'trips.db'
 
